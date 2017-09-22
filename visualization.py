7523cbd48f44c0cb9ee1c1c195d6c33b420d262c
albertoseabra <albertoseabra@users.noreply.github.com>
2017-09-22 16:09:17 +0200

    added legends to maps
    added legends to maps and created for loop to create the maps

--- visualization.py
+++ visualization.py
@@ -15,25 +15,16 @@ func = lambda x: str(x).split(' ', 1)[1]
 
 data_file['Barris'] = data_file.Barris.apply(func)
 
-# plotting with Folium
-rent_m2_map = folium.Map(location=[41.41, 2.15], zoom_start=13)
 
-rent_m2_map.choropleth(geo_data='barris_geo.json', name='choropleth', data=data_file, columns=['Barris', 'average_area'],
-                       key_on='feature.properties.N_Barri', fill_color='RdPu', fill_opacity=0.7, line_opacity=0.4)
+columns = ['average_area', 'average_rent', 'average_rent_per_m2', 'number_contracts']
+legends = ['AVERAGE AREA', 'AVERAGE RENT', 'AVERAGE RENT PER M2', 'NUMBER OF NEW CONTRACTS']
+colors = ['RdPu', 'YlGn', 'OrRd', 'YlOrRd']
 
-rent_m2_map.save('average_area.html')
+for i in range(4):
+    map = folium.Map(location=[41.41, 2.15], zoom_start=13)
 
-# n = folium.Map(location=[41.39, 2.15], zoom_start=13)
-#
-# n.choropleth(geo_data='barris_geo.json', name='choropleth', data=data_file, columns=['Barris', 'average_rent'],
-#              key_on='feature.properties.N_Barri', fill_color='YlGn', fill_opacity=0.7, line_opacity=0.4)
-#
-# n.save('average_rent.html')
-#
-# o = folium.Map(location=[41.39, 2.15], zoom_start=13)
-#
-# o.choropleth(geo_data='barris_geo.json', name='choropleth', data=data_file, columns=['Barris', 'average_rent_per_m2'],
-#              key_on='feature.properties.N_Barri', fill_color='OrRd', fill_opacity=0.7, line_opacity=0.4)
-#
-# o.save('average_rent_per_m2.html')
+    map.choropleth(geo_data='barris_geo.json', name='choropleth', data=data_file, columns=['Barris', columns[i]],
+                   key_on='feature.properties.N_Barri', fill_color=colors[i], fill_opacity=0.7, line_opacity=0.4,
+                   legend_name=legends[i], highlight= True)
 
+    map.save(str(columns[i])+'.html')
